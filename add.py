e5a809263ef65dd97ae2014b7552131ebe326271
Dwamenachrist <agyapongfrancis520@gmail.com>
2025-08-02 12:30:23 +0000

    Add simple addition and subtraction functions

---
+++ add.py
@@ -0,0 +1,32 @@
+def add_numbers(a, b):
+    """
+    Add two numbers together.
+    
+    Args:
+        a: First number
+        b: Second number
+        
+    Returns:
+        Sum of a and b
+    """
+    return a + b
+
+
+def subtract_numbers(a, b):
+    """
+    Subtract second number from first number.
+    
+    Args:
+        a: First number
+        b: Second number
+        
+    Returns:
+        Difference of a and b
+    """
+    return a - b
+
+
+if __name__ == "__main__":
+    # Example usage
+    print("Addition:", add_numbers(5, 3))
+    print("Subtraction:", subtract_numbers(10, 4)) 
